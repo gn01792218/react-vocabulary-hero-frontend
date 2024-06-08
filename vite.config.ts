f5bfc792d07e88754329bd012f3c4f5ebb5fe382
MSI\gn01792218 <gn01792218@hotmail.com>
2024-06-08 16:13:19 +0800

    [feature] add top level await

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import AutoImport from 'unplugin-auto-import/vite'
-const {resolve} = require('path')
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import AutoImport from "unplugin-auto-import/vite";
+const { resolve } = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext", //browsers can handle the latest ES features
+  },
   plugins: [
     react(),
     AutoImport({
-      imports:['react', 'react-router-dom'],
-      dts:'src/auto-imports.d.ts'
-    })
+      imports: ["react", "react-router-dom"],
+      dts: "src/auto-imports.d.ts",
+    }),
   ],
-  define:{
-    'process.env':{}
+  define: {
+    "process.env": {},
   },
-  resolve:{
-    alias:{
-      '@':resolve(__dirname,'src'),
-    }
-  }
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
