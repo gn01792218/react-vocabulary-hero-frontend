b9cdc130b8ddc68cccf6dcf41bb5c288aed31687
MSI\gn01792218 <gn01792218@hotmail.com>
2024-06-13 14:12:22 +0800

    [fix] fix route

--- src/components/RoutGuard.tsx
+++ src/components/RoutGuard.tsx
@@ -3,10 +3,10 @@ import useUser from "../hooks/user/useUser"
 export default function RoutGuard() {
 	const { user } = useUser()
 	const currentRouterPathName = location.pathname
-	console.log(currentRouterPathName)
 	return (
 		<>
 			{(() => {
+				if(!user) return
 				if (!user && currentRouterPathName !== '/Login/') return <Navigate to="/Login/" />
 				if(currentRouterPathName === '/Login/' && user) return <Navigate to="/" />
 				return <Outlet />
