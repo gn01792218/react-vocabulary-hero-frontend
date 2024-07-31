28b596ad0f930d205980cb691c1a6856ab9f6eed
MSI\gn01792218 <gn01792218@hotmail.com>
2024-07-31 19:53:44 +0800

    [update]

--- src/components/MCQ/UserMCQQuestionList.tsx
+++ src/components/MCQ/UserMCQQuestionList.tsx
@@ -1,5 +1,6 @@
 import useMCQ from "../../hooks/MCQ/useMCQ"
 import useUser from "../../hooks/user/useUser"
+import { MCQQuestion, MCQQuestionOption } from "../../types/MCQ"
 
 function UserMCQQuestionList() {
     const { user } = useUser()
@@ -16,9 +17,9 @@ function UserMCQQuestionList() {
     return (
         <ul>
             {
-                MCQs.filter(question => (question.user_id === user?.id)).map((question, index) => {
+                MCQs.filter((question:MCQQuestion) => (question?.user_id === user?.id)).map((question:MCQQuestion, index:number) => {
                     return (
-                        <li className="mb-2 flex items-start justify-between" key={question.id}>
+                        <li className="mb-2 flex items-start justify-between" key={question?.id}>
                             <div>
                                 <div className="flex">
                                     <span className="mr-1">{index + 1}.</span>
@@ -26,7 +27,7 @@ function UserMCQQuestionList() {
                                 </div>
                                 <ul>
                                     {
-                                        question.options?.map((option, index) => {
+                                        question.options?.map((option:MCQQuestionOption, index:number) => {
                                             return (
                                                 <li className="flex items-center" key={option.id}>
                                                     <span className="mr-1">({index + 1})</span>
@@ -40,7 +41,7 @@ function UserMCQQuestionList() {
                                     }
                                 </ul>
                             </div>
-                            <button className="text-red-500" onClick={()=>deleteById(question.id)}>-刪除此題</button>
+                            <button className="text-red-500" onClick={()=>deleteById(question?.id)}>-刪除此題</button>
                         </li>
                     )
                 })
