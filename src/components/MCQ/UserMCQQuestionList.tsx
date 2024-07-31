42afaa2205c08a7663572eaeed09d5eaa4be1e01
MSI\gn01792218 <gn01792218@hotmail.com>
2024-07-31 21:25:55 +0800

    [modify]

--- src/components/MCQ/UserMCQQuestionList.tsx
+++ src/components/MCQ/UserMCQQuestionList.tsx
@@ -4,7 +4,7 @@ import { MCQQuestion, MCQQuestionOption } from "../../types/MCQ"
 
 function UserMCQQuestionList() {
     const { user } = useUser()
-    const { getAll,deleteById, MCQs } = useMCQ()
+    const { getAll, deleteById, MCQs } = useMCQ()
 
     useEffect(() => {
         init()
@@ -15,38 +15,74 @@ function UserMCQQuestionList() {
     }
 
     return (
-        <ul>
-            {
-                MCQs.filter((question:MCQQuestion) => (question?.user_id === user?.id)).map((question:MCQQuestion, index:number) => {
-                    return (
-                        <li className="mb-2 flex items-start justify-between" key={question?.id}>
-                            <div>
-                                <div className="flex">
-                                    <span className="mr-1">{index + 1}.</span>
-                                    <p>{question.question}</p>
+        <section>
+            <p className="text-green-500">--------------公開的試題-------------</p>
+            <ul>
+                {
+                    MCQs.filter((question: MCQQuestion) => ((question?.user_id === user?.id) && question.share)).map((question: MCQQuestion, index: number) => {
+                        return (
+                            <li className="mb-2 flex items-start justify-between" key={question?.id}>
+                                <div>
+                                    <div className="flex">
+                                        <span className="mr-1">{index + 1}.</span>
+                                        <p>{question.question}</p>
+                                    </div>
+                                    <ul>
+                                        {
+                                            question.options?.map((option: MCQQuestionOption, index: number) => {
+                                                return (
+                                                    <li className="flex items-center" key={option.id}>
+                                                        <span className="mr-1">({index + 1})</span>
+                                                        <p className="mr-1">{option.content}</p>
+                                                        {
+                                                            option.is_answer ? <p className="text-rose-500 text-xs">正解</p> : ''
+                                                        }
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
                                 </div>
-                                <ul>
-                                    {
-                                        question.options?.map((option:MCQQuestionOption, index:number) => {
-                                            return (
-                                                <li className="flex items-center" key={option.id}>
-                                                    <span className="mr-1">({index + 1})</span>
-                                                    <p className="mr-1">{option.content}</p>
-                                                    {
-                                                        option.is_answer ? <p className="text-rose-500 text-xs">正解</p> : ''
-                                                    }
-                                                </li>
-                                            )
-                                        })
-                                    }
-                                </ul>
-                            </div>
-                            <button className="text-red-500" onClick={()=>deleteById(question?.id)}>-刪除此題</button>
-                        </li>
-                    )
-                })
-            }
-        </ul>
+                                <button className="text-red-500" onClick={() => deleteById(question?.id)}>-刪除此題</button>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <p className="mt-2 text-gray-400">-------------未公開的試題-------------</p>
+               <ul>
+                {
+                    MCQs.filter((question: MCQQuestion) => ((question?.user_id === user?.id) && !question.share)).map((question: MCQQuestion, index: number) => {
+                        return (
+                            <li className="mb-2 flex items-start justify-between" key={question?.id}>
+                                <div>
+                                    <div className="flex">
+                                        <span className="mr-1">{index + 1}.</span>
+                                        <p>{question.question}</p>
+                                    </div>
+                                    <ul>
+                                        {
+                                            question.options?.map((option: MCQQuestionOption, index: number) => {
+                                                return (
+                                                    <li className="flex items-center" key={option.id}>
+                                                        <span className="mr-1">({index + 1})</span>
+                                                        <p className="mr-1">{option.content}</p>
+                                                        {
+                                                            option.is_answer ? <p className="text-rose-500 text-xs">正解</p> : ''
+                                                        }
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                                <button className="text-red-500" onClick={() => deleteById(question?.id)}>-刪除此題</button>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </section>
     )
 }
 export default UserMCQQuestionList
